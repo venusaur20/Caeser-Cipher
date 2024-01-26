bde6c54065b4437b24ded8db308af2450dba5c97
venusaur20 <144698164+venusaur20@users.noreply.github.com>
2024-01-26 12:05:34 +0200

    finished main bulk of project; just needs to be cleaned

--- encrypter.py
+++ encrypter.py
@@ -1,33 +1,56 @@
-mode_choice = input("Decrypt or Encrypt? ").lower
+mode_choice = input("Decrypt or Encrypt? ").upper()
 
-if list(mode_choice)[0] == "d":
-    mode = ("decrypt")
-elif list(mode_choice)[0] == "e":
+if "N" in mode_choice:
     mode = ("encrypt")
+elif "D" in mode_choice:
+    mode = ("decrypt")
 
-message = list(input(f"What would you like to {mode}? ").lower)
-shift = int(input("What is your Key? "))
+message = list(input(f"What would you like to {mode}? ").lower())
+
 
 ALPHABET = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15, "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
 
 
-def Encrypt():
+def crypt():
     message_values = []
     for letter in range(len(message)):
-        message_values.append(ALPHABET.values(message[letter]))
+        corresponding_value = message[letter]
+        if corresponding_value != " ":
+            message_values.append(ALPHABET[corresponding_value])
+        else:
+            message_values.append(" ")
     
-    print(message_values)
+    return message_values
 
-def mixer(new_message,shift):
-    for letter in range(0,len(new_message)):
-        new_message[letter] += shift
-    return message
+def mix(message_values,shift):
+    mixed_values = []
+    mixed_message = []
+
+    for letter in range(len(message_values)):
+        mixed_values.append(shift+(message_values[letter]))
+
+    for letter in range(len(mixed_values)):
+        corresponding_word = mixed_values[letter]
+        mixed_message.append(list(ALPHABET.keys())[corresponding_word-1])
+
+    return(''.join(mixed_message))
+
+def tool():
+
+    if mode == "encrypt":
+        shift = int(input("What is your Key? "))
+
+    elif mode == "decrypt":
+        shift = (int(input("What is your Key? ")))*(-1)
+
+    message_values = crypt()
+    print(mix(message_values,shift))
 
 
-#if mode == "encrypt":
-#        for letter in message:
-
-#    message == new_message
-#    new_message == []
-#
-#    print(mixer(new_message,shift))
+while True:
+    tool()
+    again = ("\n Again? ").upper()
+    if "Y" in again:
+        continue
+    else:
+        break
