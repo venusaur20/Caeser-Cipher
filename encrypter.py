8e778deaf8dab5b9baef4b9d6545cb6dc3f55c8f
venusaur20 <144698164+venusaur20@users.noreply.github.com>
2024-01-27 16:46:14 +0200

    finished everything

--- encrypter.py
+++ encrypter.py
@@ -1,25 +1,11 @@
-mode_choice = input("Decrypt or Encrypt? ").upper()
-
-if "N" in mode_choice:
-    mode = ("encrypt")
-elif "D" in mode_choice:
-    mode = ("decrypt")
-
-message = list(input(f"What would you like to {mode}? ").lower())
-
-
-ALPHABET = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15, "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+CHARACTER = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15, "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26, "A":27, "B":28, "C":29, "D":30, "E":31, "F":32, "G":33, "H":34, "I":35, "J":36, "K":37, "L":38, "M":39, "N":40, "O":41, "P":42, "Q":43, "R":44, "S":45, "T":46, "U":47, "V":48, "W":49, "X":50, "Y":51, "Z":52, "1":53, "2":54, "3":55, "4":56, "5":57, "6":58, "7":59, "8":60, "9": 61, "0":62, "`": 92, "~": 64, "!": 65, "@": 67, "#": 68, "$": 69, "%": 70, "^": 71, "^": 72, "&": 73, "*": 74, "(": 75, ")": 76, "-": 77, "_": 78, "=": 79, "+": 80, "[": 81, "{": 82, "]": 83, "}": 84, ";": 85, ":": 86, ",": 87, "<": 88, ".": 89, ">": 90, "/": 91, "?":92, " ":93}
 
 
 def crypt():
     message_values = []
     for letter in range(len(message)):
-        corresponding_value = message[letter]
-        if corresponding_value != " ":
-            message_values.append(ALPHABET[corresponding_value])
-        else:
-            message_values.append(" ")
-    
+        message_values.append(CHARACTER[message[letter]])
+
     return message_values
 
 def mix(message_values,shift):
@@ -27,29 +13,61 @@ def mix(message_values,shift):
     mixed_message = []
 
     for letter in range(len(message_values)):
-        mixed_values.append(shift+(message_values[letter]))
-
+        mixed_values.append((shift)+(message_values[letter]))
+        
     for letter in range(len(mixed_values)):
-        corresponding_word = mixed_values[letter]
-        mixed_message.append(list(ALPHABET.keys())[corresponding_word-1])
+        
+        if (mixed_values[letter]) >= (len(CHARACTER)+2):
+            mixed_values[letter] -= (len(CHARACTER)+2)
+
+        if (mixed_values[letter]) < 0:
+            mixed_values[letter] += (len(CHARACTER)+2)
+
+
+        mixed_message.append(list(CHARACTER.keys())[mixed_values[letter]-1])
 
     return(''.join(mixed_message))
 
 def tool():
-
     if mode == "encrypt":
         shift = int(input("What is your Key? "))
 
     elif mode == "decrypt":
-        shift = (int(input("What is your Key? ")))*(-1)
+        shift = int(input("What is your Key? "))*(-1)
+
+    while True:
+        
+        if shift < 0:
+            shift += (len(CHARACTER)+2)
+
+        elif shift >= (len(CHARACTER)+2):
+            shift -= (len(CHARACTER)+2)
+
+        else:
+            break
 
     message_values = crypt()
     print(mix(message_values,shift))
 
-
+#---------------Runs Code-------------------------------------
 while True:
+    
+    while True:
+        mode_choice = input("Decrypt or Encrypt? ").upper()
+
+        if "N" in mode_choice:
+            mode = ("encrypt")
+            break
+        elif "D" in mode_choice:
+            mode = ("decrypt")
+            break
+        else:
+            print("That wasn't clear. Please try retyping. \n ")
+    
+    message = list(input(f"What would you like to {mode}? "))
+
     tool()
-    again = ("\n Again? ").upper()
+    again = input("\nAgain? ").upper()
     if "Y" in again:
         continue
     else:
